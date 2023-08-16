1677cd38d593ccebe76e6bba1951cbdd9de9372b
Algernon Soriano <alger.soriano@gmail.com>
2023-08-16 19:54:13 +1000

    first_Class

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    let newDrivers = []
+    newDrivers.push(drivers[0])
+    newDrivers.push(drivers[1])
+
+    console.log(drivers)
+    return newDrivers
+}
+
+function returnLastTwoDrivers(drivers){
+    let newDrivers = []
+    let i = drivers.length
+    newDrivers.push(drivers[i-2])
+    newDrivers.push(drivers[i-1])
+
+
+    return newDrivers
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(num1){
+    return function(){
+        return num1 * 5
+    }
+}
+
+function fareDoubler(fare){
+    return fare * 2
+}
+
+function fareTripler(fare){
+    return fare * 3
+}
+
+function selectDifferentDrivers(drivers, returnDrivers){
+    return returnDrivers(drivers)
+}
